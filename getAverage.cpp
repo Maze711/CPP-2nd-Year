dc02890f90190487e1a493d16c77a522e641cef7
Maze711 <badlonmazeclarion@gmail.com>
2023-08-20 11:37:12 +0800

    For Loop and Do While Average

--- getAverage.cpp
+++ getAverage.cpp
@@ -1,25 +1,49 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
 int main()
 {
-    int y[4];
-    int sum = 0, divisor = 4;
+    // int y[4];
+    // int sum = 0, divisor = 4;
 
-    cout << "Enter 4 integers\n";
+    // cout << "Enter 4 integers\n";
 
-    for (int i = 0; i < 4; i++)
-    {
-        cout << "Integers: ";
-        cin >> y[i];
-        sum += y[i];
-    }
+    // for (int i = 0; i < 4; i++)
+    // {
+    //     cout << "Integers: ";
+    //     cin >> y[i];
+    //     sum += y[i];
+    // }
 
-    int average = sum / divisor; 
+    // int average = sum / divisor;
+
+    // cout << "Sum: " << sum << endl;
+    // cout << "Average: " << average << endl;
+
+    // return 0;
+
+    int useAsDivisors, x;
+    cout << "How many numbers you want to divided?: ";
+    cin >> useAsDivisors;
     
-    cout << "Sum: " << sum << endl;
-    cout << "Average: " << average << endl;
+    float average;
+    int OriginalDivisor = useAsDivisors;
+    int sum = 0;
 
-    return 0;
+    do
+    {
+        cout << "Enter a positive integer: ";
+        cin >> x;
+        sum += x;
+        useAsDivisors = useAsDivisors - 1;
+
+    } while (useAsDivisors > 0);
+    
+    average = sum / OriginalDivisor;
+
+    cout << "The Sum is: " << sum << endl;
+    cout << "The Average is: " << average << endl;
+
 }
+
